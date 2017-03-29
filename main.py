4ddea4af3a3cfedf0624b85f302f45f406b857a1
Liqian Ma <liqian.ma@hulu.com>
2017-03-29 18:03:32 +0800

    add system say, wangcai currently have ability to say

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-import speech_recognition as sr
+# -*- coding: utf-8 -*-
+
+import speech_recognition.base as sr
+from os import system
 
 r = sr.Recognizer()
 m = sr.Microphone()
 
-# lauguage = 'zh-CN'
-lauguage = 'en-US'
+lauguage = 'zh-CN'
+# lauguage = 'en-US'
+
+def speak_chinese(str):
+    system(('say -v Mei-Jia ' + str).encode('utf-8'))
+
+BING_KEY = "f2004b4375df4f4bab41e35b2ce1a493"
+access_token = r.connect_bing_reco_sevice(BING_KEY)
 
 try:
     print("A moment of silence, please...")
@@ -17,14 +26,22 @@ try:
         with m as source: audio = r.listen(source)
         print("Got it! Now to recognize it...")
 
+
         # recognize speech using Microsoft Bing Voice Recognition
-        BING_KEY = "f2004b4375df4f4bab41e35b2ce1a493"
         try:
-            print(" -Microsoft Bing Voice Recognition: " + r.recognize_bing(audio, key=BING_KEY, language=lauguage))
+            command = r.recognize_bing(audio, key=BING_KEY, access_token=access_token, language=lauguage)
+            print(" -Microsoft Bing Voice Recognition: " + command)
         except sr.UnknownValueError:
             print(" -Microsoft Bing Voice Recognition could not understand audio")
+            continue
         except sr.RequestError as e:
             print(" -Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+            continue
+
+        if command == u'你好':
+            speak_chinese(u'你好')
+        elif command == u'时间':
+            speak_chinese(u'我不知道')
 
         # # recognize speech using Google Speech Recognition
         # try:
